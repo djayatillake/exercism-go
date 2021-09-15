8f31a939e732f533e7c56a114a5637ea8b3aca11
davidjayatillake <david.jayatillake@ly.st>
2021-09-16 00:04:27 +0100

    card tricks initial build

---
+++ card-tricks/card_tricks.go
@@ -0,0 +1,50 @@
+package cards
+
+// GetItem retrieves an item from a slice at given position. The second return value indicates whether
+// a the given index existed in the slice or not.
+func GetItem(slice []uint8, index int) (uint8, bool) {
+	if index > len(slice)-1 || index < 0 {
+		return 0, false
+	} else {
+		return slice[index], true
+	}
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range it is be appended.
+func SetItem(slice []uint8, index int, value uint8) []uint8 {
+	if index > len(slice)-1 || index < 0 {
+		slice = append(slice, value)
+	} else {
+		slice[index] = value
+	}
+	return slice
+}
+
+// PrefilledSlice creates a slice of given length and prefills it with the given value.
+func PrefilledSlice(value, length int) []int {
+	if length <= 0 {
+		return nil
+	}
+	s := make([]int, length)
+	for i := 0; i < length; i++ {
+		s[i] = value
+	}
+	return s
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if slice == nil {
+		return nil
+	} else {
+		switch {
+		case index > len(slice)-1 || index < 0:
+			return slice
+		case index == len(slice)-1:
+			return slice[:index]
+		default:
+			return append(slice[:index], slice[index+1:]...)
+		}
+	}
+}
