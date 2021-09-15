2fa439512f7184a8b61fd92c6d53c2259ceacc09
davidjayatillake <david.jayatillake@ly.st>
2021-09-15 16:05:03 +0100

    elons toys initial build

---
+++ elons-toys/elons_toys.go
@@ -0,0 +1,54 @@
+package elon
+
+import "fmt"
+
+// Car implements a remote controlled car.
+type Car struct {
+	speed        int
+	batteryDrain int
+
+	battery  int
+	distance int
+}
+
+// Track implements a race track.
+type Track struct {
+	distance int
+}
+
+// CreateCar creates a new car with given specifications.
+func CreateCar(speed, batteryDrain int) *Car {
+	New := Car{speed: speed, batteryDrain: batteryDrain, battery: 100}
+	return &New
+}
+
+// CreateTrack creates a new track with given distance.
+func CreateTrack(distance int) Track {
+	var New Track
+	New.distance = distance
+	return New
+}
+
+// Drive drives the car one time.
+func (car *Car) Drive() {
+	if car.battery >= car.batteryDrain {
+		car.distance += car.speed
+		car.battery -= car.batteryDrain
+	}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func (car *Car) CanFinish(track Track) bool {
+	range_remaining := (car.battery / car.batteryDrain) * car.speed
+	return track.distance <= range_remaining
+}
+
+// DisplayDistance displays the distance the car is driven.
+func (car *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
+
+// DisplayBattery displays the battery level.
+func (car *Car) DisplayBattery() string {
+	return fmt.Sprint("Battery at ", car.battery, "%")
+}
