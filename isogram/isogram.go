6336fc9afed5af79b1f60d09bd6d5879fa5dab45
davidjayatillake <david.jayatillake@ly.st>
2021-09-18 22:05:31 +0100

    isogram initial build

---
+++ isogram/isogram.go
@@ -0,0 +1,25 @@
+// package isogram contains a function to check if a word is an isogram
+package isogram
+
+import "strings"
+
+// IsIsogram checks whether an input string is an isogram word
+func IsIsogram(word string) bool {
+	word_c := strings.ToLower(
+		strings.TrimSpace(
+			strings.ReplaceAll(
+				strings.ReplaceAll(word, " ", ""),
+				"-", "")))
+	if word_c == "" {
+		return true
+	}
+	letter_count := make(map[string]int)
+	word_c_s := []rune(word_c)
+	for i := range word_c_s {
+		letter_count[string(word_c_s[i])] += 1
+		if letter_count[string(word_c_s[i])] > 1 {
+			return false
+		}
+	}
+	return true
+}
