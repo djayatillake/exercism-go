b4a0359e41417ad44d634f458071238fa1dfb630
davidjayatillake <david.jayatillake@ly.st>
2021-09-19 20:31:42 +0100

    luhn build after mentoring changes

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,44 +1,30 @@
 // package luhn provides a function to validate an id number based on the Luhn algorithm
 package luhn
 
-import (
-	"strconv"
-	"strings"
-	"unicode"
-)
-
 // Valid tests whether an input string is a valid id number based on the Luhn algorithm
 func Valid(id string) bool {
-	trimmed_id := strings.TrimSpace(strings.ReplaceAll(id, " ", ""))
-	if len(trimmed_id) < 2 {
-		return false
-	}
-
-	id_slice := []rune(trimmed_id)
+	idx := len(id) - 1
 	sum := 0
-	even_len := len(id_slice)%2 == 0
+	digitco := 0
 
-	for i, v := range id_slice {
-		if !unicode.IsDigit(v) {
+	for i := idx; i >= 0; i-- {
+		r := id[i]
+		digit := (r - '0')
+		if r >= '0' && r <= '9' {
+			digitco++
+		}
+		switch {
+		case r == ' ':
+			continue
+		case digitco%2 == 1 || r == '9' || r == '0':
+			sum += int(digit)
+		case r >= '1' && r <= '4':
+			sum += int(digit << 1)
+		case r >= '5' && r <= '8':
+			sum += int(digit<<1) - 9
+		default:
 			return false
 		}
-
-		string_dig_int, _ := strconv.Atoi(string(v))
-
-		if even_len && i%2 == 0 || !even_len && i%2 == 1 {
-			sum += multi9(string_dig_int)
-		} else {
-			sum += string_dig_int
-		}
 	}
-	return sum%10 == 0
-}
-
-// multi9 helper function ONLY INTENDED TO TAKE SINGLE DIGIT INTS
-func multi9(digit int) int {
-	if digit*2 > 9 {
-		return digit*2 - 9
-	} else {
-		return digit * 2
-	}
+	return digitco >= 2 && sum%10 == 0
 }
