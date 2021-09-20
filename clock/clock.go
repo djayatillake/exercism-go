bf80a8967a5f9772001ca55813345e5d3aad87f9
davidjayatillake <david.jayatillake@ly.st>
2021-09-20 11:42:23 +0100

    clock improved build

--- clock/clock.go
+++ clock/clock.go
@@ -5,38 +5,24 @@ import "fmt"
 
 // Clock implements a clock
 type Clock struct {
-	hour int
-	min  int
+	min int
 }
 
 // New creates a clock, with hour and min inputs and outputs time as string
 func New(hour, min int) Clock {
-	min_r := min%1440 + 1440
-	hour_r := hour%24 + 24
-	clock := Clock{hour: (hour_r + min_r/60) % 24, min: min_r % 60}
-	return clock
+	min_t := (min+hour*60)%1440 + 1440
+	return Clock{min_t % 1440}
 }
 
-// dig_string helper func converts hour or mins to two digit string
-func dig_string(dig int) string {
-	if dig < 10 {
-		return "0" + fmt.Sprint(dig)
-	} else {
-		return fmt.Sprint(dig)
-	}
-}
-
-// clocktime method presents the time of a clock in "HH:MM" format
-func (clock Clock) String() string {
-	return dig_string(clock.hour) + ":" + dig_string(clock.min)
+// Stringer method presents the time of a clock in "HH:MM" format
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.min/60, c.min%60)
 }
 
 // Add method adds minutes to the time of a clock
-func (clock Clock) Add(min int) Clock {
-	min_r := min%1440 + 1440
-	clock.hour = (clock.hour + (clock.min+min_r)/60) % 24
-	clock.min = (clock.min + min_r) % 60
-	return clock
+func (c Clock) Add(min int) Clock {
+	new_min := c.min + min
+	return New(0, new_min)
 }
 
 // Subtract method adds minutes to the time of a clock
