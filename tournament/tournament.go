c771bd84f3f92cc9d223fb5bbecad1dd12c8e08b
davidjayatillake <david.jayatillake@ly.st>
2021-09-28 17:44:47 +0100

    tournament working build

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -2,7 +2,10 @@
 package tournament
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"sort"
 	"strings"
 )
@@ -14,7 +17,7 @@ type team struct {
 }
 
 // declaring league of teams
-var league = map[string]team{}
+type league map[string]team
 
 // calc method, calculates MP and P from W D L
 func (t *team) calc() {
@@ -36,7 +39,7 @@ func (t *team) update(outcome string) {
 }
 
 // result_opp for other team
-func result_opp(outcome string) (ret string) {
+func result_opp(outcome string) (err error, ret string) {
 	switch outcome {
 	case "win":
 		ret = "loss"
@@ -44,32 +47,43 @@ func result_opp(outcome string) (ret string) {
 		ret = "win"
 	case "draw":
 		ret = "draw"
+	default:
+		return errors.New("invalid outcome, needs to be win loss or draw"), ""
 	}
-	return
+	return nil, ret
 }
 
 // league_entry checks for team in league and updates or inserts as relevant
-func league_entry(t, o string) {
-	if val, ok := league[t]; ok {
+func (l league) league_entry(t, o string) {
+	if val, ok := l[t]; ok {
 		val.update(o)
-		league[t] = val
+		l[t] = val
 	} else {
 		new := team{}
 		new.update(o)
-		league[t] = new
+		l[t] = new
 	}
 }
 
 // parse_results takes a result delimited by ; and inputs it into the league
-func parse_result(result string) {
+func (l *league) parse_result(result string) (err error) {
 	result_sl := strings.Split(result, ";")
+	if len(result_sl) != 3 {
+		return errors.New("not enough values in result")
+	}
 	team_a := strings.TrimSpace(result_sl[0])
 	team_b := strings.TrimSpace(result_sl[1])
+	if team_a == team_b {
+		return errors.New("teams in result are the same")
+	}
 	outcome_a := strings.TrimSpace(result_sl[2])
-	outcome_b := result_opp(outcome_a)
-
-	league_entry(team_a, outcome_a)
-	league_entry(team_b, outcome_b)
+	err, outcome_b := result_opp(outcome_a)
+	if err != nil {
+		return err
+	}
+	l.league_entry(team_a, outcome_a)
+	l.league_entry(team_b, outcome_b)
+	return nil
 }
 
 func sortByPointsAndName(teams []team) {
@@ -85,7 +99,7 @@ func sortByPointsAndName(teams []team) {
 }
 
 // league sort accepts a league dictionary and returns a slice of teams sorted by points
-func league_sort(l map[string]team) []team {
+func league_sort(l league) []team {
 	tosort := []team{}
 	for k, v := range l {
 		t := team{name: k, MP: v.MP, W: v.W, D: v.D, L: v.L, P: v.P}
@@ -95,20 +109,30 @@ func league_sort(l map[string]team) []team {
 	return tosort
 }
 
-func Print_results(results []team) (ret string) {
-	ret += "\nTeam                           | MP |  W |  D |  L |  P\n"
+// Print_results outputs header and results to an io.Writer
+func Print_results(results []team, output io.Writer) {
+	fmt.Fprintf(output, "Team                           | MP |  W |  D |  L |  P\n")
 	for _, v := range results {
-		ret += fmt.Sprintf("%-31s| %2d |  %d |  %d |  %d |  %d\n",
+		fmt.Fprintf(output, "%-31s| %2d |  %d |  %d |  %d |  %d\n",
 			v.name, v.MP, v.W, v.D, v.L, v.P)
 	}
-	return
 }
 
 // Parse_results takes a set of results delimited by new lines and inputs it into the league and returns table
-func Tally(results string) string {
-	for _, v := range strings.Split(strings.TrimSpace(results), "\n") {
-		parse_result(v)
+func Tally(input io.Reader, output io.Writer) error {
+	scan := bufio.NewScanner(input)
+	l := league{}
+	for scan.Scan() {
+		trimmed := strings.TrimSpace(scan.Text())
+		if strings.HasPrefix(trimmed, "#") || trimmed == "" {
+			continue
+		}
+		err := l.parse_result(trimmed)
+		if err != nil {
+			return err
+		}
 	}
-	sorted_league := league_sort(league)
-	return Print_results(sorted_league)
+	sorted_league := league_sort(l)
+	Print_results(sorted_league, output)
+	return nil
 }
