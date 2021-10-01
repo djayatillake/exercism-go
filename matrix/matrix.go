3998bc2638705cf373eccea76c3b2d4a53f4446e
davidjayatillake <david.jayatillake@ly.st>
2021-10-01 16:46:31 +0100

    matrix first build

---
+++ matrix/matrix.go
@@ -0,0 +1,75 @@
+// package matrix takes a string and creates a matrix with methods
+package matrix
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+// type Matrix stores a matix of integers
+type Matrix [][]int
+
+// New creates a matrix from an input string and returns an error depending on input
+func New(input string) (*Matrix, error) {
+	new := Matrix{}
+	rows := strings.Split(input, "\n")
+	for i, row := range rows {
+		if strings.TrimSpace(row) == "" {
+			return nil, errors.New("blank row in input")
+		}
+		row_sl := strings.Split(strings.TrimSpace(row), " ")
+		if i > 0 && len(strings.Split(rows[i-1], " ")) != len(row_sl) {
+			return nil, errors.New("uneven rows")
+		}
+		col_sl := []int{}
+		for _, col := range row_sl {
+			val, err := strconv.Atoi(col)
+			if err != nil {
+				return nil, err
+			}
+			col_sl = append(col_sl, val)
+		}
+		new = append(new, col_sl)
+
+	}
+	return &new, nil
+}
+
+// Set method sets one cell of a Matrix
+func (m Matrix) Set(row, column, value int) bool {
+	if len(m) == 0 {
+		return false
+	}
+	if row > len(m)-1 || row < 0 || column > len(m[0])-1 || column < 0 {
+		return false
+	}
+	m[row][column] = value
+	return true
+}
+
+// Rows method returns the rows of a Matrix
+func (m Matrix) Rows() [][]int {
+	matrix := make([][]int, len(m))
+	for i, row := range m {
+		matrix[i] = make([]int, len(row))
+		for j, col := range row {
+			matrix[i][j] = col
+		}
+	}
+	return matrix
+}
+
+// Cols method returns the rows of a Matrix
+func (m Matrix) Cols() [][]int {
+	transpose := make([][]int, len(m[0]))
+	for i, _ := range m[0] {
+		transpose[i] = make([]int, len(m))
+	}
+	for i, row := range m {
+		for j, col := range row {
+			transpose[j][i] = col
+		}
+	}
+	return transpose
+}
