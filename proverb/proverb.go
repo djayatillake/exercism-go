ca7e69ce1efc85f3959d693dea94a521263869d7
davidjayatillake <david.jayatillake@ly.st>
2021-10-04 22:14:00 +0100

    proverb improved build

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,21 +1,16 @@
 // Package proverb contains a function which returns a proverb
 package proverb
 
-import "fmt"
-
 // Proverb returns a prover based on a string slice input
 func Proverb(rhyme []string) []string {
-	if rhyme == nil {
+	length := len(rhyme)
+	if rhyme == nil || length == 0 {
 		return []string{}
 	}
-	length := len(rhyme)
 	proverb := make([]string, length)
-	for i, v := range rhyme {
-		if i == length-1 {
-			proverb[i] = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
-		} else {
-			proverb[i] = fmt.Sprintf("For want of a %s the %s was lost.", v, rhyme[i+1])
-		}
+	for i := 0; i < length-1; i++ {
+		proverb[i] = "For want of a " + rhyme[i] + " the " + rhyme[i+1] + " was lost."
 	}
+	proverb[length-1] = "And all for the want of a " + rhyme[0] + "."
 	return proverb
 }
