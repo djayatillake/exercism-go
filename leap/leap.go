708aa03b36691d2199e175b3ec164be078368a6b
davidjayatillake <david.jayatillake@ly.st>
2021-10-04 16:45:39 +0100

    leap improved build

--- leap/leap.go
+++ leap/leap.go
@@ -3,8 +3,5 @@ package leap
 
 // IsLeapYear checks whether an int year is a leap year
 func IsLeapYear(year int) bool {
-	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
-		return true
-	}
-	return false
+	return year%4 == 0 && year%100 != 0 || year%400 == 0
 }
