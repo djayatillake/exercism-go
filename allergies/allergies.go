02fb54e88f0455247535ad464fcb13a4b71160ac
davidjayatillake <david.jayatillake@ly.st>
2021-10-13 23:01:58 +0100

    allergies improved build

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,10 +1,6 @@
 // package allergies contains a function to calculate allergies
 package allergies
 
-import (
-	"math"
-)
-
 func Allergies(score uint) (ret []string) {
 	subs := []string{
 		"cats",
@@ -17,12 +13,12 @@ func Allergies(score uint) (ret []string) {
 		"eggs",
 	}
 	score %= 256
-	for i := 0; i < 8; i++ {
-		base := uint(math.Pow(float64(2), float64(7-i)))
-		if score/base == 1 {
-			ret = append(ret, subs[i])
+	count := 7
+	for ; score > 0; score /= 2 {
+		if score%2 == 1 {
+			ret = append(ret, subs[count])
 		}
-		score %= base
+		count--
 	}
 	return
 }
