4db0558a4211841ebcdece8d8515d63b291a4910
davidjayatillake <david.jayatillake@ly.st>
2021-10-14 20:50:21 +0100

    secret handshake improved build

--- secret-handshake/secret_handshake.go
+++ secret-handshake/secret_handshake.go
@@ -4,22 +4,15 @@ package secret
 // Handshake returns a secret handshake based on a decimal input
 func Handshake(input uint) (ret []string) {
 	input %= 32
-	actions := []string{"jump", "close your eyes", "double blink", "wink"}
+	actions := []string{"wink", "double blink", "close your eyes", "jump"}
 	reverse := input/16 == 1
 	input %= 16
-	bools := make([]bool, 4)
-	i := 0
-	for j := 8; j > 0; j /= 2 {
-		bools[i] = input/uint(j) == 1
-		input %= uint(j)
-		i++
-	}
 	for i := 0; i < 4; i++ {
-		if bools[i] {
+		if input&(1<<i) > 0 {
 			if reverse {
-				ret = append(ret, actions[i])
-			} else {
 				ret = append([]string{actions[i]}, ret...)
+			} else {
+				ret = append(ret, actions[i])
 			}
 		}
 	}
