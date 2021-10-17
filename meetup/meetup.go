3e292f9e00bf1e657ca7b4916b648f4d251789ac
davidjayatillake <david.jayatillake@ly.st>
2021-10-17 17:38:00 +0100

    meetup better community solution

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -1,47 +1,24 @@
 package meetup
 
 import (
-	"fmt"
 	"time"
 )
 
-type WeekSchedule string
+type WeekSchedule int
 
-var (
-	Teenth WeekSchedule = "teenth"
-	First  WeekSchedule = "first"
-	Second WeekSchedule = "second"
-	Third  WeekSchedule = "third"
-	Fourth WeekSchedule = "fourth"
-	Last   WeekSchedule = "last"
+const (
+	First  WeekSchedule = 1
+	Second              = 8
+	Third               = 15
+	Fourth              = 22
+	Teenth              = 13
+	Last                = -6
 )
 
-const Form = "January 2 2006"
-
-func Day(week WeekSchedule, weekday time.Weekday, month time.Month, year int) int {
-	days := []int{}
-	for i := 1; i < 32; i++ {
-		date := fmt.Sprintf("%s %d %d", month.String(), i, year)
-		t, err := time.Parse(Form, date)
-		if err == nil && t.Weekday() == weekday {
-			days = append(days, i)
-		}
+func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
+	if wSched == Last {
+		month++
 	}
-	switch week {
-	case First:
-		return days[0]
-	case Second:
-		return days[1]
-	case Third:
-		return days[2]
-	case Fourth:
-		return days[3]
-	case Last:
-		return days[len(days)-1]
-	default:
-		if days[1] < 13 {
-			return days[2]
-		}
-		return days[1]
-	}
+	t := time.Date(year, month, int(wSched), 12, 0, 0, 0, time.UTC)
+	return t.Day() + int(wDay-t.Weekday()+7)%7
 }
