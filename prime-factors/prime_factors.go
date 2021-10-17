4c1e5ef3830b1608b94ebf9a103eda18b3384992
davidjayatillake <david.jayatillake@ly.st>
2021-10-17 23:56:39 +0100

    prime factors improved build

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -5,25 +5,16 @@ func Factors(n int64) []int64 {
 	if n <= 1 {
 		return []int64{}
 	}
-	num := int64(2)
-	primes := []int64{}
 	factors := []int64{}
-	for n > 1 {
-		is_prime := true
-		for _, p := range primes {
-			if num%p == 0 {
-				is_prime = false
-				break
-			}
+	for n%2 == 0 {
+		factors = append(factors, 2)
+		n /= 2
+	}
+	for num := int64(3); n > 1; num += 2 {
+		for n%num == 0 {
+			factors = append(factors, num)
+			n /= num
 		}
-		if is_prime {
-			primes = append(primes, num)
-			for n%num == 0 {
-				factors = append(factors, num)
-				n /= num
-			}
-		}
-		num++
 	}
 	return factors
 }
