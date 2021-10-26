dc20dee5b7b15766b36c81bd0556264877e74062
davidjayatillake <david.jayatillake@ly.st>
2021-10-26 20:44:46 +0100

    sublist improved build

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -3,45 +3,35 @@ package sublist
 
 type Relation string
 
-// equal determins whether two lists are equal
-func equal(list1 []int, list2 []int) bool {
-	len1, len2, c := len(list1), len(list2), 0
-	if len1 != len2 {
-		return false
-	} else if len1 == 0 {
-		return true
-	}
-	for i := 0; i < len1; i++ {
-		if list1[i] == list2[i] {
-			c++
+// is_sublist determines whether list1 is a sublist of list2
+func is_sublist(list1, list2 []int) bool {
+	len1, len2 := len(list1), len(list2)
+	for i := 0; i <= len2-len1; i++ {
+		c := 0
+		for j := 0; j < len1; j++ {
+			if list1[j] == list2[i : len1+i][j] {
+				c++
+			}
+		}
+		if c == len1 {
+			return true
 		}
 	}
-	return c == len1
+	return false
 }
 
 // Sublist tests whether a list1 is a sublist, superlist of list2 or if they are equal or unequal
-func Sublist(list1 []int, list2 []int) Relation {
+func Sublist(list1, list2 []int) Relation {
 	len1, len2 := len(list1), len(list2)
-	if equal(list1, list2) {
+	is_sub := is_sublist(list1, list2)
+	switch {
+	case len1 == 0 && len2 == 0 || is_sub && len1 == len2:
 		return "equal"
-	} else if len1 > len2 && len2 == 0 {
-		return "superlist"
-	} else if len2 > len1 && len1 == 0 {
+	case len2 > len1 && len1 == 0 || is_sub:
 		return "sublist"
+	case len1 > len2 && len2 == 0 || is_sublist(list2, list1):
+		return "superlist"
+	default:
+		return "unequal"
 	}
-
-	if len1 > len2 {
-		for i := 0; i <= len1-len2; i++ {
-			if equal(list1[i:len2+i], list2) {
-				return "superlist"
-			}
-		}
-	} else if len1 < len2 {
-		for i := 0; i <= len2-len1; i++ {
-			if equal(list2[i:len1+i], list1) {
-				return "sublist"
-			}
-		}
-	}
-	return "unequal"
 }
